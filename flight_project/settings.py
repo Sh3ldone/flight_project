628648fb832cf570938e52367e2fd3518872f289
Sheldon Edacuya <sheldonedacuya91@gmail.com>
2025-05-19 15:52:09 +0800

    Add whitenoise and static files, update settings

--- flight_project/settings.py
+++ flight_project/settings.py
@@ -99,6 +99,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # User-uploaded media files (if needed)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
