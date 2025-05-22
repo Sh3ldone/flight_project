9bf368ef9451bc43307009c0cf61ade4d1e1a9a7
Sheldon Edacuya <sheldonedacuya91@gmail.com>
2025-05-22 13:59:07 +0800

    Updated flight schedule logic and admin dashboard UI; added new migrations

--- flight_project/settings.py
+++ flight_project/settings.py
@@ -14,13 +14,7 @@ SECRET_KEY = 'django-insecure-ni2wj_1skqj71n(@-=uqae1h+vq+t$fdwjuk+ji46k(pjgfc!&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
- 
-
-ALLOWED_HOSTS = [
-    'donzflightreservation.onrender.com',
-    'localhost',
-    '127.0.0.1'
-]
+ALLOWED_HOSTS = ['*']  # You can restrict later for production
 
 
 
